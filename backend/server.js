f53cacdc9efa51518b26d3b902701e804a889c4d
captainSolo <captainsolo23@gmail.com>
2023-08-17 06:32:23 -0700

    cors

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,12 @@ const cors = require('cors')
 
 connectDB()
 
+app.use(cors())
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors())
+
 
 app.use('/api/products', require('./routes/productsRoutes'))
 app.use('/api/users', require('./routes/usersRoutes'))
