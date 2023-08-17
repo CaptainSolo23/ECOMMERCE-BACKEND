78de38caa1b0eebde733c503de21af13c1100bdb
captainSolo <captainsolo23@gmail.com>
2023-08-17 16:12:42 -0700

    reinstalled

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,9 @@ connectDB()
 
 
 const app = express()
-
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-
-
-app.use(cors());
 app.use('/api/products', require('./routes/productsRoutes'))
 app.use('/api/users', require('./routes/usersRoutes'))
 app.use('/api/order', require('./routes/ordersRoutes'))
